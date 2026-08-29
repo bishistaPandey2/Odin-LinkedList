52613207ddef4d8f8b878f896f816179760813e8
Bishista Pandey <bishistapandey@gmail.com>
2026-08-29 14:39:33 +0000

    Create linked list class and append node.

---
+++ index.js
@@ -0,0 +1,27 @@
+class LinkedList(){
+	constructor(head){
+		this.head = null
+	}
+	append(value){
+		const newNode = new Node(value)
+
+		if(this.head == null){
+			this.head = newNode
+			return;
+		}
+
+		let current = this.head
+		while (current.next !== null){
+			current = current.next
+		}
+
+		current.next = newNode
+		return this
+	}
+}
+class Node(){
+	constructor(value){
+		this.value = null
+		this.nextNode = null
+	}
+}
