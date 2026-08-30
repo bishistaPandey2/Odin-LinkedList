0fd971faef45ab430005f96af6ded1ca171300d1
Bishista Pandey <bishistapandey@gmail.com>
2026-08-30 05:22:52 +0000

    Add size code to return the size of the linked list

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 class LinkedList(){
-	constructor(head){
+	constructor(){
 		this.head = null
 	}
 	append(value){
 		const newNode = new Node(value)
 
-		if(this.head == null){
+		if(this.head === null){
 			this.head = newNode
-			return;
+			return
 		}
 
 		let current = this.head
-		while (current.next !== null){
+		while (current.nextNode !== null){
 			current = current.next
 		}
 
 		current.next = newNode
 		return this
+	}
+
+	prepend(value){
+		const newNode = new Node(value)
+
+		if(this.head === null){
+			this.head = newNode
+			return
+		}
+
+		newNode.nextNode = this.head;	
+		this.head = newNode
+
+		return this
+	}
+
+	size(){
+		let counter = 1
+		let currentNode = this.head
+		if(currentNode === null){
+			return undefined
+		}
+
+		while(currentNode.nextNode !== null){
+			counter++
+			currentNode = currentNode.nextNode
+		}
+
+		return counter
 	}
 }
 class Node(){
