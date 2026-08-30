51d2d236f5110c1e0f994d7a132302b0b2925e4a
Bishista Pandey <bishistapandey@gmail.com>
2026-08-30 10:10:55 +0000

    Repopulate the list with nodes

--- test.js
+++ test.js
@@ -3,12 +3,13 @@ import {LinkedList} from "./index.js"
 
 const list = new LinkedList()
 
-list.append("Apple")
-list.append("Banana")
-list.append("Pine")
-list.append("Guava")
-
-list.prepend("Grapes")
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.prepend("eagle")
 
 const head =list.headNode()
 let nThNode = list.at(4)
