14266e9a9e55973d997062fd82ea294654fd1fb7
Bishista Pandey <bishistapandey@gmail.com>
2026-08-30 10:58:16 +0000

    Add tst to pop the first element.

--- test.js
+++ test.js
@@ -18,13 +18,13 @@ const nThNode = list.at(4)
 const nodeValue = "snake"
 const thereOrNot = list.contains(nodeValue)
 const nodeIndex = list.findIndex(nodeValue)
-// const stringList = list.toString()
+list.pop()
 
-// console.log(`${stringList}`)
-console.log(nodeIndex)
+console.log(`The ${nodeValue} is at index: ${nodeIndex}`)
 console.log(`The ${nodeValue} exists there ?= ${thereOrNot}`)
 console.log(`The size of the linked list is ${list.size()}`)
 console.log(`This is ${nThNode}th node = ${nThNode}`)
-console.log("This is linked list = " + list)
 console.log("This is head = " + head)
 console.log("This is tail = " + tail)
+
+console.log(list.toString())
