fc45c26376289e6d538f4c1d7d0467f66f54660d
Bishista Pandey <bishistapandey@gmail.com>
2026-08-30 10:20:15 +0000

    Test case to find the size of linked list.

--- test.js
+++ test.js
@@ -10,11 +10,13 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 list.prepend("eagle")
+list.append("babboon")
 
 const head =list.headNode()
 const tail = list.tail()
 let nThNode = list.at(4)
 
+console.log(`The size of the linked list is ${list.size()}`)
 console.log(`This is ${nThNode}th node = ${nThNode}`)
 console.log("This is linked list = " + list)
 console.log("This is head = " + head)
